c249a2cc971af5d7d335ffc52f559722fab2a27f
eric.woo <wadahana@gmail.com>
2021-07-24 15:19:34 +0000

    change font size to 22

--- config.mk
+++ config.mk
@@ -4,11 +4,11 @@ VERSION = 0.8.4
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr/local
+PREFIX = /opt/suckless
 MANPREFIX = $(PREFIX)/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/include/X11
+X11LIB = /usr/lib/X11
 
 PKG_CONFIG = pkg-config
 
